d42a6eeab892105b4512fc4a5db2d2866fe879a7
Kiyo <kakabori@andrew.cmu.edu>
2014-11-07 20:07:11 -0500

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import sys
 
 def read_frm_dot_dat(filename):
     pixel = []; scaling = []; cz = []; qz = []; sigma_scaling = []; sigma_cz = [];
@@ -8,7 +9,7 @@ def read_frm_dot_dat(filename):
     for line in lines:
         # This ignores an empty line
         line = line.rstrip()
-        if not line: 
+        if not line:
             continue
         p, s, c, q, ss, sc = line.split()
         p = int(p)
@@ -17,8 +18,8 @@ def read_frm_dot_dat(filename):
         q = float(q)
         ss = float(ss)
         sc = float(sc)
-        pixel.append(p) 
-        scaling.append(s) 
+        pixel.append(p)
+        scaling.append(s)
         cz.append(c)
         qz.append(q)
         sigma_scaling.append(ss)
@@ -32,7 +33,7 @@ def apply_Lorentz_correction(f):
     return f
 
 
-def apply_absorption_correction():
+def apply_absorption_correction(f):
     pass
     
 
@@ -40,11 +41,13 @@ def output_to_smp_file():
     pass
     
     
-def remove_errorneous_data_points():
-    pass
+def remove_errorneous_data_points(qz, scale):
+    """qz and scale are both lists of data points"""
+    for i in xrange(len(qz)):
+        if scale[i] == 
     
     
-def smooth_form_factor():
+def smooth_form_factor(f):
     pass
     
 
@@ -52,6 +55,15 @@ def average_form_factor()
     pass
 
    
-
- 
-
+if __name__ == '__main__':
+    filenames = []
+    all_qz = []
+    all_scale = []
+    for arg in sys.argv:
+        filenames.append(arg)
+    for f in filenames:
+        px, scale, cz, qz, sig_scale, sig_cz = read_frm_dot_dat(f)
+        all_qz.append(qz)
+        all_scale.append(scale)
+    for i in xrange(len(all_qz)):
+        remove_errorneous_data_points(all_qz[i], all_scale[i])
